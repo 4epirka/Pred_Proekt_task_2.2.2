03ebb3295ac414e19dfa0cd5e5eb400d571c0d20
4epirka <ivantar2000@mail.ru>
2025-09-17 16:16:53 +0300

    Доработка

--- src/main/java/web/services/CarServiceImpl.java
+++ src/main/java/web/services/CarServiceImpl.java
@@ -2,6 +2,7 @@ package web.services;
 
 
 import org.springframework.stereotype.Service;
+import web.dao.CarDao;
 import web.dao.CarDaoImpl;
 import web.model.Car;
 
@@ -10,15 +11,15 @@ import java.util.List;
 @Service
 public class CarServiceImpl implements CarService {
 
-    private final CarDaoImpl carDaoImpl;
+    private final CarDao carDao;
 
-    public CarServiceImpl(CarDaoImpl carDaoImpl) {
-        this.carDaoImpl = carDaoImpl;
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
     }
 
     @Override
     public List<Car> getCars(List<Car> unused, int count) {
-        List<Car> cars = carDaoImpl.getAllCars();
+        List<Car> cars = carDao.getAllCars();
         if (count <= 0 || count >= cars.size()) {
             return cars;
         }
